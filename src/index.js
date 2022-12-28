8240431dff75f082c0ba3937b67e7cf4b25d9e67
storm2107 <113288447+storm2107@users.noreply.github.com>
2022-12-28 20:19:40 +0200

    e

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries.js';
+import { debounce } from 'lodash';
+import countryListItem from './teamplates/contry_list.hbs';
+import country from './teamplates/country.hbs';
+
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+const DEBOUNCE_DELAY = 300;
+
+input.addEventListener('input', debounce(inputCountry, DEBOUNCE_DELAY));
+
+function inputCountry(el) {
+  const countryName = el.target.value.trim();
+
+  fetchCountries(countryName)
+    .then(data => {
+      //   console.log(data);
+      if (data.length >= 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length <= 10 && data.length >= 2) {
+        countryList.innerHTML = countryListItem(data);
+        if (countryInfo.childNodes) {
+          while (countryInfo.firstChild) {
+            countryInfo.removeChild(countryInfo.firstChild);
+          }
+        }
+      } else {
+        if (countryList.childNodes) {
+          while (countryList.firstChild) {
+            countryList.removeChild(countryList.firstChild);
+          }
+        }
+        countryInfo.innerHTML = country(data);
+      }
+    })
+    .catch(Notiflix.Notify.failure('Oops, there is no country with that name'));
+}
